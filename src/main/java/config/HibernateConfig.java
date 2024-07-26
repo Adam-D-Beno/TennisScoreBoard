57e84c3cf628c329788805207d7308be5a937b6f
ПК_Home <wrofdt@ya.ru>
2024-07-26 10:19:16 +0300

    refactoring HibernateConfig, make singleton Double-Checked Locking add getSessionFactoryMethod

--- src/main/java/config/HibernateConfig.java
+++ src/main/java/config/HibernateConfig.java
@@ -2,18 +2,16 @@ package config;
 
 import lombok.Getter;
 import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
 public class HibernateConfig {
     private static volatile HibernateConfig instance;
-    @Getter
-    private static final SessionFactory sessionFactory;
 
-    static {
-        sessionFactory = buildSessionFactory();
-    }
+    private static SessionFactory sessionFactory;
+
 
     private HibernateConfig() {
     }
@@ -23,14 +21,20 @@ public class HibernateConfig {
             synchronized (HibernateConfig.class) {
                 if (instance == null) {
                     instance = new HibernateConfig();
+                    sessionFactory = buildSessionFactory();
                 }
             }
         }
         return instance;
     }
 
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
     private static SessionFactory buildSessionFactory() {
-        try(SessionFactory sessionFactory = getConfiguration().buildSessionFactory()) {
+        try {
+            SessionFactory sessionFactory = getConfiguration().buildSessionFactory();
             return sessionFactory;
         } catch (HibernateException e) {
             throw new ExceptionInInitializerError(e);
