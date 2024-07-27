0354de0ccbfd754d50a651042e388a55489f993f
ПК_Home <wrofdt@ya.ru>
2024-07-27 22:19:26 +0300

    refactoring MatchScoreController

--- src/main/java/Controller/MatchScoreController.java
+++ src/main/java/Controller/MatchScoreController.java
@@ -5,19 +5,31 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import service.NewMatchService;
 
 import java.io.IOException;
+import java.util.UUID;
 
-@WebServlet(name = "match-score", value = "/match-score")
+@WebServlet(name = "match-score", urlPatterns = "/match-score")
 public class MatchScoreController extends HttpServlet {
+    private NewMatchService newMatchService;
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    public MatchScoreController() {
+        this.newMatchService = new NewMatchService();
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer playerId = getPlayerId(req);
+        UUID matchId = getMatchId(req);
+    }
+
+    private Integer getPlayerId(HttpServletRequest req) {
+        //todo validation is null, is Alfabet
+        return Integer.parseInt(req.getParameter("playerId"));
+    }
+
+    private UUID getMatchId(HttpServletRequest req) {
+        return UUID.nameUUIDFromBytes(req.getParameter("uuid").getBytes());
     }
 }
