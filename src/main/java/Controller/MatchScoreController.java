f9138a65c8c8e722cf06eff4ff92f650d7421807
ПК_Home <wrofdt@ya.ru>
2024-07-28 11:44:23 +0300

    Add validation MatchScoreController

--- src/main/java/Controller/MatchScoreController.java
+++ src/main/java/Controller/MatchScoreController.java
@@ -5,31 +5,45 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import service.MatchScoreCalculationService;
 import service.NewMatchService;
+import service.OngoingMatchesService;
+import validation.PlayerValidate;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @WebServlet(name = "match-score", urlPatterns = "/match-score")
 public class MatchScoreController extends HttpServlet {
-    private NewMatchService newMatchService;
+
+    private final OngoingMatchesService ongoingMatchesService;
+    private final PlayerValidate playerValidate = PlayerValidate.getInstance();
 
     public MatchScoreController() {
-        this.newMatchService = new NewMatchService();
+        this.ongoingMatchesService = new OngoingMatchesService();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Integer playerId = getPlayerId(req);
         UUID matchId = getMatchId(req);
+
     }
 
     private Integer getPlayerId(HttpServletRequest req) {
-        //todo validation is null, is Alfabet
+        if (playerValidate.isEmptyOrNull(req.getParameter("playerId"))) {
+            throw new IllegalArgumentException("Player id is empty or null");
+        }
+        if (playerValidate.isNumber(req.getParameter("playerId"))) {
+            throw new IllegalArgumentException("Player id is not numeric");
+        }
         return Integer.parseInt(req.getParameter("playerId"));
     }
 
     private UUID getMatchId(HttpServletRequest req) {
+       if (playerValidate.isEmptyOrNull(req.getParameter("uuid"))) {
+           throw new IllegalArgumentException("UUID is empty or null");
+        }
         return UUID.nameUUIDFromBytes(req.getParameter("uuid").getBytes());
     }
 }
