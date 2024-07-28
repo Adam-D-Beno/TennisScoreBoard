d2c5d354721a43c2d204e4110203c41166298f53
ПК_Home <wrofdt@ya.ru>
2024-07-28 21:02:22 +0300

    create methods addPoints / addGames / addSets

--- src/main/java/service/MainGameService.java
+++ src/main/java/service/MainGameService.java
@@ -1,4 +1,32 @@
 package service;
 
+import entity.Match;
+import model.MatchScoreModel;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public class MainGameService {
+    private final MatchScoreModel scoreModel = MatchScoreModel.getInstance();
+
+    public void beginGame(Integer playerId, UUID matchId) {
+
+
+    }
+
+    private Optional<Match> getMatch(UUID matchId) {
+        return scoreModel.getMatch(matchId);
+    }
+
+    private void addPoints(Integer playerId) {
+
+    }
+
+    private void addGames(Integer playerId) {
+
+    }
+
+    private void addSets(Integer playerId) {
+
+    }
 }
