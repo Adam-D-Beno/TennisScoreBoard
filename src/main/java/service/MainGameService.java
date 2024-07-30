b3419f368d5e1483163dfe187466d05cfc475ee2
ПК_Home <wrofdt@ya.ru>
2024-07-30 20:15:15 +0300

    add delete methods: scoringGames, checkIncreaseAddPoints, checkIncreaseAddGames, checkIncreaseAddSets

--- src/main/java/service/MainGameService.java
+++ src/main/java/service/MainGameService.java
@@ -9,51 +9,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class MainGameService {
+    private final MatchScoreCalculationService matchScoreCalculationService;
+    private final FinishedMatchesPersistenceService finishedMatchesPersistenceService;
+
+    public MainGameService() {
+        matchScoreCalculationService = new MatchScoreCalculationService();
+        finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
+    }
 
     public void beginGame(Integer playerId, UUID matchId) {
 
-        scoringGames(playerId, getMatch(matchId));
+        matchScoreCalculationService.scoringGames(playerId, getMatch(matchId));
     }
 
 
-
-    private void scoringGames(Integer playerId, Match match) {
-        Player firstPlayer = match.getFirstPlayer();
-        Player secondPlayer = match.getSecondPlayer();
-
-        //todo validation exist in bd
-        if (firstPlayer.getId().equals(playerId)) {
-            checkIncreaseAddPoints(firstPlayer);
-
-        }
-
-        if (secondPlayer.getId().equals(playerId)) {
-            checkIncreaseAddPoints(secondPlayer);
-        }
-
-    }
-
-    private void checkIncreaseAddPoints(Player player) {
-        player.addPoints();
-
-        if (player.getPoints() >= 40) {
-            checkIncreaseAddGames(player);
-        }
-    }
-
-    private void checkIncreaseAddGames(Player player) {
-        player.addGames();
-
-        if (player.getGames() >= 6) {
-            checkIncreaseAddSets(player);
-        }
-    }
-
-    private void checkIncreaseAddSets(Player player) {
-        player.addSets();
-
-
-    }
 
     private Match getMatch(UUID matchId) {
         return MatchScoreModel.getInstance().getMatch(matchId)
