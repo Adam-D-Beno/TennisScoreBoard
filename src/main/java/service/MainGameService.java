8dfbde3872f4c10870d674fdfd85db16ed9a56c8
ПК_Home <wrofdt@ya.ru>
2024-07-30 20:02:04 +0300

    add methods: scoringGames, checkIncreaseAddPoints, checkIncreaseAddGames, checkIncreaseAddSets

--- src/main/java/service/MainGameService.java
+++ src/main/java/service/MainGameService.java
@@ -1,32 +1,62 @@
 package service;
 
 import entity.Match;
+import entity.Player;
 import model.MatchScoreModel;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 import java.util.UUID;
 
 public class MainGameService {
-    private final MatchScoreModel scoreModel = MatchScoreModel.getInstance();
 
     public void beginGame(Integer playerId, UUID matchId) {
 
+        scoringGames(playerId, getMatch(matchId));
+    }
+
+
+
+    private void scoringGames(Integer playerId, Match match) {
+        Player firstPlayer = match.getFirstPlayer();
+        Player secondPlayer = match.getSecondPlayer();
+
+        //todo validation exist in bd
+        if (firstPlayer.getId().equals(playerId)) {
+            checkIncreaseAddPoints(firstPlayer);
+
+        }
+
+        if (secondPlayer.getId().equals(playerId)) {
+            checkIncreaseAddPoints(secondPlayer);
+        }
 
     }
 
-    private Optional<Match> getMatch(UUID matchId) {
-        return scoreModel.getMatch(matchId);
+    private void checkIncreaseAddPoints(Player player) {
+        player.addPoints();
+
+        if (player.getPoints() >= 40) {
+            checkIncreaseAddGames(player);
+        }
     }
 
-    private void addPoints(Integer playerId) {
+    private void checkIncreaseAddGames(Player player) {
+        player.addGames();
+
+        if (player.getGames() >= 6) {
+            checkIncreaseAddSets(player);
+        }
+    }
+
+    private void checkIncreaseAddSets(Player player) {
+        player.addSets();
+
 
     }
 
-    private void addGames(Integer playerId) {
-
-    }
-
-    private void addSets(Integer playerId) {
-
+    private Match getMatch(UUID matchId) {
+        return MatchScoreModel.getInstance().getMatch(matchId)
+                .orElseThrow(() -> new EntityNotFoundException("Object match is not found in scoreModel"));
     }
 }
