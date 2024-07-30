ba699de7f20d7565f25b25bd58d77da0a72478f6
ПК_Home <wrofdt@ya.ru>
2024-07-30 19:56:57 +0300

    add method isExist

---
+++ src/main/java/validation/MatchValidate.java
@@ -0,0 +1,18 @@
+package validation;
+
+import entity.Match;
+
+public class MatchValidate {
+    private final static MatchValidate INSTANCE = new MatchValidate();
+
+    private MatchValidate() {
+    }
+
+    public MatchValidate getInstance() {
+        return INSTANCE;
+    }
+
+    public boolean isExist(Match match) {
+        return match == null;
+    }
+}
