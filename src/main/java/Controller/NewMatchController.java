cc9495f87620405e14f61dc90fc475eb5979af3e
ПК_Home <wrofdt@ya.ru>
2024-07-30 19:59:27 +0300

    refactoring add method: getPlayerName

--- src/main/java/Controller/NewMatchController.java
+++ src/main/java/Controller/NewMatchController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import service.NewMatchService;
+import validation.PlayerValidate;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -13,16 +14,23 @@ import java.util.UUID;
 @WebServlet(name = "new-match", urlPatterns = "/new-match")
 public class NewMatchController extends HttpServlet {
     private final NewMatchService newMatchService;
-
+    private final PlayerValidate playerValidate = PlayerValidate.getInstance();
     public NewMatchController() {
         this.newMatchService = new NewMatchService();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String firstPlayerName = req.getParameter("player1");
-        String secondPlayerName = req.getParameter("player2");
-        UUID match_id = newMatchService.CreateNewMatch(firstPlayerName, secondPlayerName);
-        resp.sendRedirect("/match-score.jsp?uuid=$" + match_id);
+        String firstPlayerName = getPlayerName(req, "player1");
+        String secondPlayerName = getPlayerName(req, "player2");
+        UUID match_id = newMatchService.createNewMatch(firstPlayerName, secondPlayerName);
+        resp.sendRedirect("/match-score?uuid=$" + match_id);
+    }
+
+    private String getPlayerName(HttpServletRequest req, String playerName) {
+        if (playerValidate.isEmptyOrNull(req.getParameter(playerName))) {
+            throw new IllegalArgumentException("Player name is empty or null");
+        }
+        return req.getParameter(playerName);
     }
 }
