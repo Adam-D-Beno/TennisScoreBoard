487d0ce5287d4e89983bfc5c4aaa5299c6e99dfd
ПК_Home <wrofdt@ya.ru>
2024-07-30 19:58:25 +0300

    add index and constraint

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -6,17 +6,41 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.function.Supplier;
 
-@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "players")
+@Table(name = "players", indexes = @Index(name = "name_index", columnList = "name", unique = true))
 @Builder
 public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PRIVATE)
     private Integer id;
-    @Column(name = "name")
+    @Column(name = "name", unique = true, nullable = false)
     private String name;
+
+    @Transient
+    @Setter(AccessLevel.PRIVATE)
+    private int points;
+
+    @Transient
+    @Setter(AccessLevel.PRIVATE)
+    private int games;
+
+    @Transient
+    @Setter(AccessLevel.PRIVATE)
+    private int sets;
+
+    public void addPoints() {
+        this.points += 15;
+    }
+
+    public void addGames() {
+        this.games += 1;
+    }
+
+    public void addSets() {
+        this.sets += 1;
+    }
 }
