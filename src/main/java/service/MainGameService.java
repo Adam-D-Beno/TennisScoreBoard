b46f449ed80cbff45c534ac84d5fa302cdc8f34b
ПК_Home <wrofdt@ya.ru>
2024-07-30 21:18:05 +0300

    refactoring add OngoingMatchesService

--- src/main/java/service/MainGameService.java
+++ src/main/java/service/MainGameService.java
@@ -11,17 +11,18 @@ import java.util.UUID;
 public class MainGameService {
     private final MatchScoreCalculationService matchScoreCalculationService;
     private final FinishedMatchesPersistenceService finishedMatchesPersistenceService;
+    private final OngoingMatchesService ongoingMatchesService;
 
     public MainGameService() {
         matchScoreCalculationService = new MatchScoreCalculationService();
         finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
+        ongoingMatchesService = new OngoingMatchesService();
     }
 
     public void beginGame(Integer playerId, UUID matchId) {
-
         matchScoreCalculationService.scoringGames(playerId, getMatch(matchId));
+        finishedMatchesPersistenceService.save(getMatch(matchId));
     }
-
 
 
     private Match getMatch(UUID matchId) {
