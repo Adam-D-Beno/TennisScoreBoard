226cff11e9d93af35ce30d913ed440da5112effd
ПК_Home <wrofdt@ya.ru>
2024-07-30 21:27:53 +0300

    refactoring add UUID

--- src/main/java/entity/Match.java
+++ src/main/java/entity/Match.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,6 +16,10 @@ public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Setter
+    @Transient
+    private UUID uuid;
 
     @ManyToOne
     @JoinColumn(name = "firstPlayerId")
