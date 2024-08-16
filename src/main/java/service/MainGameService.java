14cd429ace09fe0cb38bdb598fe2da1b0722f64a
ПК_Home <wrofdt@ya.ru>
2024-08-16 20:27:57 +0300

    refactoring

--- src/main/java/service/MainGameService.java
+++ src/main/java/service/MainGameService.java
@@ -1,5 +1,7 @@
 package service;
 
+import model.MatchScore;
+
 import java.util.UUID;
 
 public class MainGameService {
@@ -10,12 +12,16 @@ public class MainGameService {
     public MainGameService() {
         this.matchScoreCalculationService = new MatchScoreCalculationService();
         this.finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
-        this. ongoingMatchesService = new OngoingMatchesService();
+        this.ongoingMatchesService = new OngoingMatchesService();
     }
 
     public void beginGame(Integer playerId, UUID matchId) {
         matchScoreCalculationService.ScoreCalculation(playerId, matchId);
-        finishedMatchesPersistenceService.save(matchId);
-        ongoingMatchesService.removeMatchScores(matchId);
+        ongoingMatchesService.getMatchScores(matchId)
+                .filter(MatchScore::isMatchEnd)
+                .ifPresent(matchScore -> {
+                    finishedMatchesPersistenceService.save(matchId);
+                    ongoingMatchesService.removeMatchScores(matchId);
+                });
     }
 }
