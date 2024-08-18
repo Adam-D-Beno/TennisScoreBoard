d2b2bdf26ef897bc14b669f26fbec88230edeefd
ПК_Home <wrofdt@ya.ru>
2024-08-18 23:14:58 +0300

    Add context listener

---
+++ src/main/java/context/AppContextListenerMainGameService.java
@@ -0,0 +1,15 @@
+package context;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import service.MainGameService;
+
+@WebListener
+public class AppContextListenerMainGameService implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("mainGameService", new MainGameService());
+    }
+}
