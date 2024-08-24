a820908571a53e1a9e0ff974f7020ad9cc8470e3
ПК_Home <wrofdt@ya.ru>
2024-08-24 22:00:45 +0300

    add AppContextListenerBuildSessionFactory

---
+++ src/main/java/context/AppContextListenerBuildSessionFactory.java
@@ -0,0 +1,14 @@
+package context;
+
+import config.HibernateConfig;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListenerBuildSessionFactory implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("sc", HibernateConfig.getInstance());
+    }
+}
