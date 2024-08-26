7a9c1b296ec6698eef9c8d6408fc77bff8df9eb5
ПК_Home <wrofdt@ya.ru>
2024-08-26 23:59:55 +0300

    refactoring FinishedMatchesPersistenceService

--- src/main/java/service/FinishedMatchesPersistenceService.java
+++ src/main/java/service/FinishedMatchesPersistenceService.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 public class FinishedMatchesPersistenceService {
     private final SpecMatchRepository<Match, Integer> matchRepository;
     private final OngoingMatchesService ongoingMatchesService;
+    private static final int MATCHES_ON_PAGE = 5;
 
     public FinishedMatchesPersistenceService(OngoingMatchesService ongoingMatchesService) {
         this.matchRepository = new MatchRepository();
@@ -24,8 +25,16 @@ public class FinishedMatchesPersistenceService {
         executionTransaction(toMatch(matchId));
     }
 
-    public List<Match> getMatchesByPlayerName(String playerName) {
-        return null;
+    public List<Match> getMatchesByPlayerName(int pageNumber, String winPlayerName) {
+        Session session = HibernateConfig
+                .getInstance()
+                .getSessionFactory()
+                .getCurrentSession();
+
+        session.beginTransaction();
+        List<Match> matches = matchRepository.getByWinPlayerName(MATCHES_ON_PAGE, pageNumber, winPlayerName, session);
+        session.getTransaction().commit();
+        return matches;
     }
 
     private void executionTransaction(Match match) {
@@ -54,5 +63,17 @@ public class FinishedMatchesPersistenceService {
                         .winner(matchScore.getWinner())
                         .build())
         ).orElseThrow(() -> new EntityNotFoundException("Object match is not found in scoreModel"));
+    }
+
+    public long getTotalPage() {
+        Session session = HibernateConfig
+                .getInstance()
+                .getSessionFactory()
+                .getCurrentSession();
+
+        session.beginTransaction();
+        long totalMatches = matchRepository.getTotalMatches(session);
+        session.getTransaction().commit();
+        return totalMatches;
     }
 }
