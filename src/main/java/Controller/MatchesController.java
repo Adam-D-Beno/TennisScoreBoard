3c207a906978f32202637d11a61818f4775fb505
ПК_Home <wrofdt@ya.ru>
2024-08-27 00:00:26 +0300

    refactoring MatchesController

--- src/main/java/Controller/MatchesController.java
+++ src/main/java/Controller/MatchesController.java
@@ -7,20 +7,39 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import service.MainGameService;
+import validation.MatchValidate;
 
 import java.io.IOException;
 
 @WebServlet(name = "matches", urlPatterns = "/matches")
 public class MatchesController extends HttpServlet {
     private MainGameService mainGameService;
+    private MatchValidate matchValidate;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         mainGameService = (MainGameService) config.getServletContext().getAttribute("mainGameService");
+        matchValidate = MatchValidate.getInstance();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int page = getPage(req);
+        String playerName = getPlayerName(req);
+        req.setAttribute("content", mainGameService.getMatchesPlayed(page, playerName));
+        req.getRequestDispatcher("/finished-matches.jsp").forward(req,resp);
 
     }
+
+    private String getPlayerName(HttpServletRequest req) {
+        matchValidate.isEmptyOrNull(req.getParameter("filter_by_player_name"));
+        return req.getParameter("filter_by_player_name");
+    }
+
+    private int getPage(HttpServletRequest req) {
+        matchValidate.isEmptyOrNull(req.getParameter("page"));
+        matchValidate.isNumber(req.getParameter("page"));
+        return Integer.parseInt(req.getParameter("page"));
+    }
+
 }
