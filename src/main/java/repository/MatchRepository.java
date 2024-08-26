c64cc16a6933584a66242c25fd56a7c679835d0c
ПК_Home <wrofdt@ya.ru>
2024-08-27 00:00:40 +0300

    refactoring MatchRepository

--- src/main/java/repository/MatchRepository.java
+++ src/main/java/repository/MatchRepository.java
@@ -1,11 +1,13 @@
 package repository;
 
-import lombok.NoArgsConstructor;
+
 import model.Match;
-import lombok.RequiredArgsConstructor;
-import model.Player;
 import org.hibernate.Session;
 
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -24,7 +26,22 @@ public class MatchRepository implements SpecMatchRepository<Match, Integer> {
     }
 
     @Override
-    public List<Match> getByWinPlayerName(String winPlayerName, Session session) {
-        return Collections.emptyList();
+    public List<Match> getByWinPlayerName(int pageSize, int pageNumber, String winPlayerName, Session session) {
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<Match> criteriaQuery = criteriaBuilder.createQuery(Match.class);
+        Root<Match> matchRoot = criteriaQuery.from(Match.class);
+        criteriaQuery.select(matchRoot);
+
+        Query query = session.createQuery(criteriaQuery);
+
+        query.setFirstResult((pageNumber - 1) * pageSize);
+        query.setMaxResults(pageSize);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public long getTotalMatches(Session session) {
+        return (long) session.createQuery("SELECT COUNT (m.id) FROM Match m").uniqueResult();
     }
 }
