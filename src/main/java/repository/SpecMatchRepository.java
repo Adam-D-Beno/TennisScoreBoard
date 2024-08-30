6a41b697013e3c926b154053579c48960cba88e8
ПК_Home <wrofdt@ya.ru>
2024-08-30 08:34:06 +0300

    refactor SpecMatchRepository.java

--- src/main/java/repository/SpecMatchRepository.java
+++ src/main/java/repository/SpecMatchRepository.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface SpecMatchRepository<E, K extends Serializable> extends Repository<E, K>{
-     List<E> getByWinPlayerName(int pageSize, int pageNumber, String winPlayerName, Session session);
+     List<E> getByPlayerName(int pageSize, int pageNumber, String winPlayerName, Session session);
      long getTotalMatches(Session session);
 
 }
